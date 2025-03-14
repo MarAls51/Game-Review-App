9076f913dd12eeb1754b9930f1f8d074884c803a
MA <wilcomeep123@gmail.com>
2025-03-14 05:54:02 -0400

    no cors in node

--- backend_container/backend/server.js
+++ backend_container/backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const logger = require("./utils/logger");
